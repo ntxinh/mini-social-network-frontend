4ebd77cfe4509fa7c3496159d60b589baa53c569
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2018-02-24 19:24:26 +0700

    :sparkles: Clean source with new layout

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -12,12 +12,6 @@ import {
 
 const appRoutes = [
     { path: "/dashboard", sidebarName: "Dashboard", navbarName: "Material Dashboard", icon: Dashboard, component: DashboardPage },
-    { path: "/user", sidebarName: "User Profile", navbarName: "Profile", icon: Person, component: UserProfile },
-    { path: "/table", sidebarName: "Table List", navbarName: "Table List", icon: ContentPaste, component: TableList },
-    { path: "/typography", sidebarName: "Typography", navbarName: "Typography", icon: LibraryBooks, component: Typography },
-    { path: "/icons", sidebarName: "Icons", navbarName: "Icons", icon: BubbleChart, component: Icons },
-    { path: "/maps", sidebarName: "Maps", navbarName: "Map", icon: LocationOn, component: Maps },
-    { path: "/notifications", sidebarName: "Notifications", navbarName: "Notifications", icon: Notifications, component: NotificationsPage },
     { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
 ];
 
